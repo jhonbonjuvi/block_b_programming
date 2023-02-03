7cc820baee2f018aea98f4ed075e16316435c9ad
jhonbonjuvi <johnbonjuvijumarito@gmail.com>
2023-02-03 15:07:34 +0800

    updated descriptive ibutang guys!

---
+++ main.py
@@ -0,0 +1,8 @@
+
+x = 1
+print("hello world")
+y = input("Please...")
+print("the answer is" + y)
+
+
+# system.out.println("")
---
+++ samle.c
@@ -0,0 +1,8 @@
+include <iostream>
+using namespace std;
+int main(){int x=1;
+
+cout<<"enter value:"<<endl;cin>>x
+    
+    return 0;
+}
